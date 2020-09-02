629584330a6b172b450ee31b4c938d4cee074b4d
sergeishay <sergeishay@gmail.com>
2020-09-02 18:50:10 +0300

    mobx install

--- src/views/BroadcastRoom.jsx
+++ src/views/BroadcastRoom.jsx
@@ -3,16 +3,30 @@ import socketIOClient from 'socket.io-client'
 import Hero from '../components/Hero'
 import Content from '../components/Content'
 import SCRCarousel from '../components/Carousel/Carousel'
+import axios from 'axios'
 const ENDPOINT = 'http://0ed93a492609.ngrok.io'
-
+const ID = 321;
 const Homepage = () => {
   const [response, setResponse] = useState('')
+
+
   useEffect(() => {
-    const socket = socketIOClient(ENDPOINT)
-    socket.on('FromAPI', data => {
-      setResponse(data)
-    })
+    async function getData() {
+      const response = await axios.get(`http://0ed93a492609.ngrok.io/broadcast/534534534` ,{params : {ID}})
+      console.log(response.data)
+      if (!response.data.error) {
+        const socket = socketIOClient(ENDPOINT)
+        socket.on('FromAPI', data => {
+          setResponse(data)
+        })
+      } else {
+        console.log(response.data.error)
+      }
+    }
+    getData()
+    // socket.emit("toAPI" , )
   }, [])
+
 
   return <Fragment>Socket response: {response}</Fragment>
 }
